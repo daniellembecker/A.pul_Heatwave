5e4ded533e0e475b2a949ef7c5fbc16c3f00ce70
daniellembecker <danielle_becker@uri.edu>
2024-09-27 18:28:54 -0400

    gomwu and topgo scripts

--- bioinformatics/scripts/goMWU/GO_MWU.R
+++ bioinformatics/scripts/goMWU/GO_MWU.R
@@ -32,9 +32,9 @@ source("gomwu.functions.R")
 
 gomwuStats(input, goDatabase, goAnnotations, goDivision,
 	perlPath="perl", # replace with full path to perl executable if it is not in your system's PATH already
-	largest=0.5,  # a GO category will not be considered if it contains more than this fraction of the total number of genes
-	smallest=5,   # a GO category should contain at least this many genes to be considered
-	clusterCutHeight=0.25, # threshold for merging similar (gene-sharing) terms. See README for details.
+	largest=0.8,  # a GO category will not be considered if it contains more than this fraction of the total number of genes
+	smallest=2,   # a GO category should contain at least this many genes to be considered
+	clusterCutHeight=0.4, # threshold for merging similar (gene-sharing) terms. See README for details.
 #	Alternative="g" # by default the MWU test is two-tailed; specify "g" or "l" of you want to test for "greater" or "less" instead. 
 #	Module=TRUE,Alternative="g" # un-remark this if you are analyzing a SIGNED WGCNA module (values: 0 for not in module genes, kME for in-module genes). In the call to gomwuPlot below, specify absValue=0.001 (count number of "good genes" that fall into the module)
 #	Module=TRUE # un-remark this if you are analyzing an UNSIGNED WGCNA module 
